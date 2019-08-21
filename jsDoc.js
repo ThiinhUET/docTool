281f5554a63849c0e701671e29e47327699ad572
ThiinhUET <thinhlevan2015@gmail.com>
2019-08-21 05:09:42 +0000

    release

--- jsDoc.js
+++ jsDoc.js
@@ -7,7 +7,7 @@ var myJsdoc = 'https://github.com/ThiinhUET/Jsdoc_gen';
 var files = fs.readdirSync(path);
 
 for(let i = 0; i < files.length; i++){
-    var dir = path + '/' + files[i];
-    git(dir).clone(myJsdoc);
-    console.log("Clone sub repo done !");
+	var dir = path + '/' + files[i];
+	 git(dir).clone(myJsdoc);
+	console.log("Clone sub repo done !");
 }
